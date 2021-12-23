a79e0ef5b89cc97a45823f70a34a2ec01d49aa30
garretthensley <garrettmichaelhensley@gmail.com>
2021-12-23 12:12:53 -0500

    geocoder

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const errorHandler = require('./middleware/error')
+const connectDB = require('./config/db')
 
 // load env vars
 dotenv.config({ path: './config/config.env' })
 
+// Connect to database
+connectDB()
+
+// Route Files
+const bootcamps = require('./routes/bootcamps')
+
 const app = express()
+
+// Body parser - included with express
+app.use(express.json())
+
+// Dev Logging middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'))
+}
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
+const server = app.listen(
 	PORT,
-	console.log(`Server is running in ${process.env.NODE_ENV} mode on port`)
+	console.log(
+		`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue
+			.bold.bgWhite
+	)
 )
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`.red)
+	// Close server & exit process
+	server.close(() => process.exit(1))
+})
